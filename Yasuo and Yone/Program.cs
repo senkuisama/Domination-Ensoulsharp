e6ee85b0738fa971e3663cc3fc9b8090bd34bd74
senkuisama <56258340+senkuisama@users.noreply.github.com>
2020-08-06 11:58:13 +0700

    skinchange
    that have been a long time

--- Yasuo and Yone/Program.cs
+++ Yasuo and Yone/Program.cs
@@ -46,6 +46,47 @@ namespace ConsoleApp
 
             if (ObjectManager.Player.CharacterName == "Yone")
                 ("Yone Script make by FunnySlayer, Good luck").YoneLoad();
+
+            try
+            {
+                Game.Print("SkinHack v1.0.1");
+                Game.Print("Thanks for help 011110001");
+                Game.Print("Creator: emredeger");
+
+                var menu = new Menu("skinhack", "SkinHack", true);
+
+                var champs = menu.Add(new Menu("Champions", "Champions"));
+                var allies = champs.Add(new Menu("Allies", "Allies"));
+                var enemies = champs.Add(new Menu("Enemies", "Enemies"));
+
+                foreach (var hero in GameObjects.Heroes.Where(h => !h.CharacterName.Equals("Ezreal")))
+                {
+                    var champMenu = new Menu(hero.CharacterName, hero.CharacterName);
+                    champMenu.Add(new MenuSlider("SkinIndex", "Skin Index", 1, 1, 70));
+                    champMenu.GetValue<MenuSlider>("SkinIndex").ValueChanged += (s, e) =>
+                    {
+                        Console.WriteLine($"[SKINHACK] Skin ID: {champMenu.GetValue<MenuSlider>("SkinIndex").Value}");
+                        GameObjects.Heroes.ForEach(
+                            p =>
+                            {
+                                if (p.CharacterName == hero.CharacterName)
+                                {
+                                    Console.WriteLine($"[SKINHACK] Changed: {hero.CharacterName}");
+                                    p.SetSkin(champMenu.GetValue<MenuSlider>("SkinIndex").Value);
+                                }
+                            });
+                    };
+
+                    var rootMenu = hero.IsAlly ? allies : enemies;
+                    rootMenu.Add(champMenu);
+                }
+
+                menu.Attach();
+            }
+            catch
+            {
+
+            }
         }
     }
     internal static class Yasuo_GodLike
